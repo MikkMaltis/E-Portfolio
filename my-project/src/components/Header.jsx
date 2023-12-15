f1dd75ec4140c34aaf1d77dc0eea673204a7dfbf
MikkMaltis <maltismikk@gmail.com>
2023-12-15 12:12:07 +0200

    commit

--- my-project/src/components/Header.jsx
+++ my-project/src/components/Header.jsx
@@ -54,25 +54,27 @@ const Header = () => {
 
             {/* Socials */}
             <div className='flex fixed flex-col top-[35%] left-0 pl-2 mt-6'>
-                <ul>
-                    <li>
-                        <a href='#'>
-                           <img className="w-12 mb-2" src={Mail}/> 
-                        </a>
+                <div className='h-full rounded-2xl bg-color6'>
+                    <ul>
+                        <li>
+                            <a href='#'>
+                                <img className="w-12 mb-2" src={Mail}/> 
+                            </a>
                         
-                        <a href="https://www.facebook.com/p/Mikk-Maltis-100009786704562/" target="_blank" rel="noopener noreferrer">
-                            <img className="w-12 mb-2" src={Facebook}/>
-                        </a>
+                            <a href="https://www.facebook.com/p/Mikk-Maltis-100009786704562/" target="_blank" rel="noopener noreferrer">
+                                <img className="w-12 mb-2" src={Facebook}/>
+                            </a>
 
-                        <a href="https://www.linkedin.com/in/mikk-maltis-132742268/?originalSubdomain=ee" target="_blank" rel="noopener noreferrer">
-                            <img className="w-12 mb-2" src={LinkedIn}/>
-                        </a>
+                            <a href="https://www.linkedin.com/in/mikk-maltis-132742268/?originalSubdomain=ee" target="_blank" rel="noopener noreferrer">
+                                <img className="w-12 mb-2" src={LinkedIn}/>
+                            </a>
 
-                        <a href="https://github.com/MikkMaltis" target="_blank" rel="noopener noreferrer">
-                            <img className="w-12 mb-2" src={Github}/>
-                        </a>
-                    </li>
-                </ul>
+                            <a href="https://github.com/MikkMaltis" target="_blank" rel="noopener noreferrer">
+                                <img className="w-12 mb-2" src={Github}/>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
     );
