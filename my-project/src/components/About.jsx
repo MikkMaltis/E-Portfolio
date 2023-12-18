7c644cfab5baf7783037230f67a41b0a90c2830c
MikkMaltis <maltismikk@gmail.com>
2023-12-18 18:02:51 +0200

    .

--- my-project/src/components/About.jsx
+++ my-project/src/components/About.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import mountains from '../media/mountains.jpg';
 
 const About = () => {
   return (
     <div className='w-full px-6 py-12 flex justify-center items-center font-Cabin text-[#f3f4f6]'>
       <div className="bg-color4 w-[88%] rounded py-6 px-6">
-        <h1 className="text-6xl py-6 px-6">About me</h1>
+        <div>
+          <img className='' src={mountains} alt="Background Mountains" />
+          <h1 className="text-6xl py-6 px-6 font-Kanit flex justify-center">About me</h1>
+        </div>
+
         <div className="flex justify-center">
-          <p className="text-center text-2xl">I am a 17 year old web developer from Estonia. I have been doing web development for about 1 and half year now. I have worked with React, TailwindCSS, HTML, CSS, JavaScript, Node.js, Express.js, Python, PHP, SQL, MariaDB, Git and GitHub. I am currently studying at the Kuressaare Regional Training Centre, where I am studying software development.</p>
+          <p className="text-center text-2xl font-Kanit leading-10">I am a 17 year old web developer from Estonia. I have been doing web development for about 1 and half year now. I have worked with React, TailwindCSS, HTML, CSS, JavaScript, Node.js, Express.js, Python, PHP, SQL, MariaDB, Git and GitHub. I am currently studying at the Kuressaare Regional Training Centre, where I am studying software development.</p>
         </div>
       </div>
     </div>
--- my-project/src/components/Projects.jsx
+++ my-project/src/components/Projects.jsx
@@ -2,26 +2,32 @@ import React from 'react';
 import Sorteerijama from '../media/Sorteerijama.png';
 import Tailwind from '../media/Tailwind.png';
 import Aistartuptools from '../media/Aistartuptools.png';
+import Apple from '../media/Apple.png';
 
 const Projects = () => {
   return (
     <div className='px-6 py-12 flex justify-center items-center font-Cabin text-[#f3f4f6]'>
-      <div className='bg-color6 rounded py-6 px-6 rounded-2xl text-center'>
-        <h1 className="text-6xl py-6 px-6 text-color4">Projects</h1>
+      <div className='bg-color9 rounded py-6 px-6 rounded-2xl text-center grid grid-cols-4 gap-4 flex place-items-center mx-20'>
+        <h1 className="text-6xl py-6 px-6 text-color4 col-span-4">Projects</h1>
+
           <a href="https://github.com/MikkMaltis/2023-9.20.2023-projekt">
+            <h1>SorteeriJAMA group project</h1>
             <img src={Sorteerijama} alt="" />
           </a>
           <a href="https://github.com/MikkMaltis/Tailwind-website-with-templates">
+          <h1>Tailwind student company template page</h1>
             <img src={Tailwind} alt="" />
           </a>
           <a href="https://github.com/MikkMaltis/Ai-startup-tool">
+          <h1>AI startup tool template page</h1>
             <img src={Aistartuptools} alt="" />
           </a>
-          <a href="">
-            <img src="" alt="" />
+          <a href="https://github.com/MikkMaltis/Apple">
+          <h1>Apple copy website</h1>
+            <img src={Apple} alt="" />
           </a>
+        </div>
       </div>
-    </div>
   )
 }
 
